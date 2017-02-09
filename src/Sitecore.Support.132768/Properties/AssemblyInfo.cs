6e825fc6bf7ddb42c2af1af81f68c870b1bd4c58
DK\pk <pk@sitecore.net>
2017-02-09 15:56:20 +0200

    The Sitecore.Data.DataProviders.Sql.SqlDataProvider.ResolvePath method has ben overriden to fix the bug. An additional private method IsIDPath was added, which checks the entire path for the Long ID format.

---
+++ src/Sitecore.Support.132768/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.132768")]
+[assembly: AssemblyProduct("Sitecore.Support.132768")]
+[assembly: ComVisible(false)]
